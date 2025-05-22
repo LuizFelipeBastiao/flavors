125b87bba51655da6132df6a3ecde24b20a6f78e
Luiz Felipe Bastião <lbastiao@alunos.utfpr.edu.br>
2025-05-22 00:01:43 -0300

    FEITO ARQUIVOS ENV

--- flutter_flavors/lib/main_dev.dart
+++ flutter_flavors/lib/main_dev.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flavors/models/flavor_config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 void main(){
- 
+  
+  // Load environment variables from .env file
+  dotenv.load(fileName: ".env.dev");
    FlavorConfig.appFlavor = Flavor.development;
   
   runApp(MyApp(flavorConfig: FlavorConfig(),),);
--- flutter_flavors/lib/main_prod.dart
+++ flutter_flavors/lib/main_prod.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'models/flavor_config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 
 void main() {
+  dotenv.load(fileName: ".env.prod");
   FlavorConfig.appFlavor = Flavor.production;
   
   runApp(MyApp(flavorConfig: FlavorConfig(),),);
